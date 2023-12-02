276c7caa716d85587437d3e79a24952628b75ff3
Jo3yG <nonuser31@yahoo.com>
2023-12-01 20:06:48 -0600

    added new character to controllers/api

---
+++ controllers/api/newCharacter.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {User, Character} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req,res) => {
+    try{
+        const newCharacter = await Character.create({
+            charName: req.body.charName ,
+            description: req.body.description,
+            regMoves: req.body.reqMoves,
+            finalSmash: req.body.finalSmash,
+            gameSeries: req.body.gameSeries,
+            firstApp: req.body.firstApp,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newCharacter);
+    }catch (err) {
+        res.status(400).json(err);
+      }
+});
