ba00c3b2f7c06ee16c17391522e9542184704fb2
Jo3yG <nonuser31@yahoo.com>
2023-12-06 17:11:22 -0600

    merging

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const User = require('./user');
 const Character = require('./character');
 
-User.hasOne(Character, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-Character.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+//User.hasOne(Character, {
+//  foreignKey: 'user_id',
+  //onDelete: 'CASCADE'
+//});
+//Character.belongsTo(User, {
+  //foreignKey: 'user_id'
+//});
 
 module.exports = { User, Character };
